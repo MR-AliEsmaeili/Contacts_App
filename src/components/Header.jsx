f4e7b8b2a84b596fd3636ea7cbbe57b9ed0c18ec
علی اسماعیلی <a.esmaeali@fpc.local>
2024-11-26 11:21:08 +0330

    add icon to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import styles from "./Header.module.css";
+import imp from "../assets/fav.webp";
 const Header = () => {
   return (
     <>
       <div className={styles.container}>
-        <h2>اپلیکیشن مدیریت مخاطبین</h2>
+        <h2>
+          <p>اپلیکیشن مدیریت مخاطبین</p>
+          <img src={imp} />
+        </h2>
       </div>
     </>
   );
