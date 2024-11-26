bf81e80308f575e1bd74d8b780fe0ea6c2cc1cbf
علی اسماعیلی <a.esmaeali@fpc.local>
2024-11-26 11:08:34 +0330

    correct the fav icon

--- src/components/ViweContacts.jsx
+++ src/components/ViweContacts.jsx
@@ -1,30 +1,29 @@
 import styled from "./ViweContacts.module.css";
-const ViweContacts = ({
-  deleteContact,
-  contacts = { id, name, lastname, email, phone },
-}) => {
+const ViweContacts = ({ deleteContact, contacts = [] }) => {
   return (
     <div className={styled.container}>
       <h3>لیست مخاطبین:</h3>
+      {contacts.length === 0 ? (
+        <p>مخاطبی برای نمایش وجود ندارد</p>
+      ) : (
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>
+              <p>نام : {contact.name}</p>
+              <p> نام خانوادگی : {contact.lastname}</p>
 
-      <ul>
-        {contacts.map((contact) => (
-          <li key={contact.id}>
-            <p>نام : {contact.name}</p>
-            <p> نام خانوادگی : {contact.lastname}</p>
-
-            <p>
-              <span>✉</span>
-              {contact.email}
-            </p>
-
-            <p>
-              <span>📞</span> {contact.phone}
-            </p>
-            <button onClick={() => deleteContact(contact.id)}>🗑</button>
-          </li>
-        ))}
-      </ul>
+              <p>
+                <span>💬</span>
+                {contact.email}
+              </p>
+              <p>
+                <span>📱</span> {contact.phone}
+              </p>
+              <button onClick={() => deleteContact(contact.id)}>حذف 🗑</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
